c3ae3c66dfe293cf188fe806051155565a02d970
NewbieDronny <65925109+NewbieDronny@users.noreply.github.com>
2022-04-13 01:00:08 +0300

    1
    1

---
+++ js/main.js
@@ -0,0 +1,90 @@
+swiper = new Swiper('.covers', {
+    // Optional parameters
+    //Ориентация (вертикальная, горизонтальная)
+    direction: 'horizontal',
+    loop: true,
+    //Количество слайдов для показа
+    slidesPerView: 3,
+    autoplay: {
+        delay: 4000
+    },
+    lazy: true,
+    lazy: {
+        loadOnTransitionStart: false,
+        loadPervNext: false
+    },
+    watchSlidersProgress: true,
+    watchSlidersVisibility: true,
+
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+
+});
+swiper = new Swiper('.mybutter', {
+    // Optional parameters
+    //Ориентация (вертикальная, горизонтальная)
+    direction: 'horizontal',
+    loop: true,
+    //Количество слайдов для показа
+    slidesPerView: 3,
+    autoplay: {
+        delay: 5000
+    },
+    lazy: true,
+    lazy: {
+        loadOnTransitionStart: false,
+        loadPervNext: false
+    },
+    watchSlidersProgress: true,
+    watchSlidersVisibility: true,
+
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+
+});
+swiper = new Swiper('.dd', {
+    // Optional parameters
+    //Ориентация (вертикальная, горизонтальная)
+    direction: 'horizontal',
+    loop: true,
+    //Количество слайдов для показа
+    slidesPerView: 3,
+    autoplay: {
+        delay: 7000
+    },
+    lazy: true,
+    lazy: {
+        loadOnTransitionStart: false,
+        loadPervNext: false
+    },
+    watchSlidersProgress: true,
+    watchSlidersVisibility: true,
+
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+
+});
